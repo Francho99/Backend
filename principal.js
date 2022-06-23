93fc4987f06ae6f6ac3ad572e72da1507f78c7b0
Franco Choca <francochoca199@gmail.com>
2022-06-22 22:14:11 -0300

    new file:   archivo.txt 	new file:   package.json 	new file:   principal.js

---
+++ principal.js
@@ -0,0 +1,22 @@
+const fs = require('fs')
+
+
+const archivo = './archivo.txt'
+
+class Contenedor{
+    constructor(archivo){
+        this.archivo = archivo
+    }
+
+    save(obejto){
+        fs.appendFile
+    }
+
+    getById(){}
+
+    getAll(){}
+
+    deleteById(){}
+
+    deleteAll(){}
+}
